4e4761034c4f86318b6e2033ab99df84f6d375df
Jlisowskyy <Jlisowskyy@gmail.com>
2024-03-23 01:20:48 +0100

    Prepared material table functions

--- include/Evaluation/BoardEvaluator.h
+++ include/Evaluation/BoardEvaluator.h
@@ -5,12 +5,8 @@
 #ifndef BOARDEVALUATOR_H
 #define BOARDEVALUATOR_H
 
-#include <iomanip>
-
 #include "../EngineTypeDefs.h"
-#include "../MoveGeneration/BlackPawnMap.h"
 #include "../MoveGeneration/KingMap.h"
-#include "../MoveGeneration/WhitePawnMap.h"
 
 /*      Collection of evaluation function
  *
@@ -31,215 +27,161 @@ struct BoardEvaluator
     // ------------------------------
 
     BoardEvaluator() = default;
+
     ~BoardEvaluator() = default;
 
     // ------------------------------
     // Class interaction
     // ------------------------------
 
-    [[nodiscard]] static int32_t DefaultFullEvalFunction(const Board& bd, const int color)
+    [[nodiscard]] static int32_t DefaultFullEvalFunction(const Board&bd, const int color)
     {
         const int whiteEval = NaiveEvaluation3(bd);
         return color == WHITE ? whiteEval : -whiteEval;
     }
 
     // function uses only material to evaluate passed board
-    [[nodiscard]] static int32_t NaiveEvaluation1(const Board& bd)
+    [[nodiscard]] static int32_t PlainMaterialEvaluation(const Board&bd);
+
+    [[nodiscard]] static int32_t NaiveEvaluation2(const Board&bd);
+
+    [[nodiscard]] static int32_t NaiveEvaluation3(const Board&bd);
+
+    [[nodiscard]] static int32_t Evaluation1(const Board&bd)
     {
-        int eval = 0;
-
-        for (size_t i = 0; i < Board::BoardsCount; ++i)
-            eval += CountOnesInBoard(bd.boards[i]) * BasicFigureValues[i];
-
-        return eval;
-    }
-
-    [[nodiscard]] static int32_t NaiveEvaluation2(const Board& bd)
-    {
-        int eval = 0;
-
-        // iterate through boards
-        for (size_t bInd = 0; bInd < Board::BoardsCount; ++bInd)
-        {
-            // extract figures board
-            uint64_t figs = bd.boards[bInd];
-
-            // iterate through figures
-            while (figs)
-            {
-                const int figPos = ExtractMsbPos(figs);
-
-                // sum costs offseted by position
-                eval += CostsWithPositionsIncluded[bInd][figPos];
-
-                // remove processed figures
-                figs ^= maxMsbPossible >> figPos;
-            }
-        }
-
-        return eval;
-    }
-
-    [[nodiscard]] static int32_t NaiveEvaluation3(const Board& bd)
-    {
-        int eval = 0;
-
-        // iterate through boards
-        for (size_t bInd = 0; bInd < Board::BoardsCount; ++bInd)
-        {
-            // extract figures board
-            uint64_t figs = bd.boards[bInd];
-
-            // iterate through figures
-            while (figs)
-            {
-                const int figPos = ExtractMsbPos(figs);
-
-                // sum costs offseted by position
-                eval += CostsWithPositionsIncluded[bInd][figPos];
-
-                // remove processed figures
-                figs ^= maxMsbPossible >> figPos;
-            }
-        }
-
-        // add bonuses for covering pawns
-        eval = _applyBonusForCoveredPawns(bd, eval);
-
-        return eval;
-    }
-
-    void static DisplayCosts()
-    {
-        for (size_t bd = 0 ; bd < Board::BoardsCount; ++bd)
-        {
-            for (size_t pos = 0; pos < Board::BoardFields; ++pos)
-            {
-                if (pos % 8 == 0) std::cout << std::endl;
-
-                std::cout << std::setw(6) << CostsWithPositionsIncluded[bd][pos] << " , ";
-            }
-
-            std::cout << std::endl;
-        }
+        return 1;
     }
 
     // ------------------------------
     // Private class methods
     // ------------------------------
-   private:
-    static int32_t _applyBonusForCoveredPawns(const Board& bd, int32_t eval)
-    {
-        const uint64_t covereWPawns = WhitePawnMap::GetAttackFields(bd.boards[wPawnsIndex]) & bd.boards[wPawnsIndex];
-        eval += CountOnesInBoard(covereWPawns) * CoveredWPawnBonus;
 
-        const uint64_t coveredBPawns = BlackPawnMap::GetAttackFields(bd.boards[bPawnsIndex]) & bd.boards[bPawnsIndex];
-        eval += CountOnesInBoard(coveredBPawns) * CoveredBPawnBonus;
+private:
+    static int32_t _applyBonusForCoveredPawns(const Board&bd, int32_t eval);
 
-        return eval;
-    }
+    static size_t _getMaterialBoardIndex(const Board&bd);
+
+    static size_t _getMaterialBoardIndex(size_t P, size_t N, size_t B, size_t R, size_t Q,
+                                         size_t p, size_t n, size_t b, size_t r,
+                                         size_t q);
 
     // ------------------------------
     // Class fields
     // ------------------------------
-   public:
-    using CostArrayT = std::array<std::array<int32_t, Board::BoardFields>, Board::BoardsCount>;
+public:
+    using CostArrayT = std::array<std::array<int16_t, Board::BoardFields>, Board::BoardsCount>;
 
-    static constexpr int32_t CoveredWPawnBonus = 5;
-    static constexpr int32_t CoveredBPawnBonus = -5;
+    // for each color and each figure including lack of figure /pawn/knigh/bishop/rook/queen/
+    static constexpr size_t MaterialTableSize = 9 * 9 * 3 * 3 * 3 * 3 * 3 * 3 * 2 * 2;
 
-    static constexpr int32_t BasicFigureValues[]{
-        100,    // Pawn
-        330,    // Knight
-        350,    // Bishop
-        500,    // Rook
-        900,    // Queen
-        10000,  // king
-        -100,   // Pawn
-        -330,   // Knight
-        -350,   // Bishop
-        -500,   // Rook
-        -900,   // Queen
-        -10000  // king
+    static constexpr size_t BlackPawnCoef = MaterialTableSize / 9;
+    static constexpr size_t WhitePawnCoef = BlackPawnCoef / 9;
+    static constexpr size_t BlackKnightCoef = WhitePawnCoef / 3;
+    static constexpr size_t WhiteKnightCoef = BlackKnightCoef / 3;
+    static constexpr size_t BlackBishopCoef = WhiteKnightCoef / 3;
+    static constexpr size_t WhiteBishopCoef = BlackBishopCoef / 3;
+    static constexpr size_t BlackRookCoef = WhiteBishopCoef / 3;
+    static constexpr size_t WhiteRookCoef = BlackRookCoef / 3;
+    static constexpr size_t BlackQueenCoef = WhiteRookCoef / 2;
+    static constexpr size_t WhiteQueenCoef = BlackQueenCoef / 2;
+
+    using MaterialArrayT = std::array<int16_t, MaterialTableSize>;
+
+    static constexpr int16_t CoveredWPawnBonus = 5;
+    static constexpr int16_t CoveredBPawnBonus = -5;
+
+    static constexpr int16_t BasicFigureValues[]{
+        100, // Pawn
+        330, // Knight
+        350, // Bishop
+        500, // Rook
+        900, // Queen
+        10000, // king
+        -100, // Pawn
+        -330, // Knight
+        -350, // Bishop
+        -500, // Rook
+        -900, // Queen
+        -10000 // king
     };
 
-    static constexpr int32_t BasicBlackPawnPositionValues[]
+    static constexpr int16_t BasicBlackPawnPositionValues[]
     {
-        0,  0,  0,  0,  0,  0,  0,  0,
-       50, 50, 50, 50, 50, 50, 50, 50,
-       10, 10, 20, 30, 30, 20, 10, 10,
-       10, 10, 20, 25, 25, 10, 10, 10,
-        5,  7, 10, 20, 20, 10,  7,  5,
-        5,  5,  5,  0,  0,  5,  5,  5,
-        5,  8,  8,-20,-20,  8,  8,  5,
-        0,  0,  0,  0,  0,  0,  0,  0
+        0, 0, 0, 0, 0, 0, 0, 0,
+        50, 50, 50, 50, 50, 50, 50, 50,
+        10, 10, 20, 30, 30, 20, 10, 10,
+        10, 10, 20, 25, 25, 10, 10, 10,
+        5, 7, 10, 20, 20, 10, 7, 5,
+        5, 5, 5, 0, 0, 5, 5, 5,
+        5, 8, 8, -20, -20, 8, 8, 5,
+        0, 0, 0, 0, 0, 0, 0, 0
     };
 
-    static constexpr int32_t BasicBlackKnightPositionValues[]
+    static constexpr int16_t BasicBlackKnightPositionValues[]
     {
-        -50,-40,-30,-30,-30,-30,-40,-50,
-        -40,-20,  0,  0,  0,  0,-20,-40,
-        -30,  0, 10, 15, 15, 10,  0,-30,
-        -30,  5, 15, 20, 20, 15,  5,-30,
-        -30,  0, 15, 20, 20, 15,  0,-30,
-        -30,  5, 10, 15, 15, 10,  5,-30,
-        -40,-20,  0,  5,  5,  0,-20,-40,
-        -50,-40,-30,-30,-30,-30,-40,-50,
+        -50, -40, -30, -30, -30, -30, -40, -50,
+        -40, -20, 0, 0, 0, 0, -20, -40,
+        -30, 0, 10, 15, 15, 10, 0, -30,
+        -30, 5, 15, 20, 20, 15, 5, -30,
+        -30, 0, 15, 20, 20, 15, 0, -30,
+        -30, 5, 10, 15, 15, 10, 5, -30,
+        -40, -20, 0, 5, 5, 0, -20, -40,
+        -50, -40, -30, -30, -30, -30, -40, -50,
     };
 
-    static constexpr int32_t BasicBlackBishopPositionValues[]
+    static constexpr int16_t BasicBlackBishopPositionValues[]
     {
-        -20,-10,-10,-10,-10,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5, 10, 10,  5,  0,-10,
-        -10,  5,  5, 10, 10,  5,  5,-10,
-        -10,  0, 10, 10, 10, 10,  0,-10,
-        -10, 10, 10, 10, 10, 10, 10,-10,
-        -10,  5,  0,  0,  0,  0,  5,-10,
-        -20,-10,-10,-10,-10,-10,-10,-20,
+        -20, -10, -10, -10, -10, -10, -10, -20,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -10, 0, 5, 10, 10, 5, 0, -10,
+        -10, 5, 5, 10, 10, 5, 5, -10,
+        -10, 0, 10, 10, 10, 10, 0, -10,
+        -10, 10, 10, 10, 10, 10, 10, -10,
+        -10, 5, 0, 0, 0, 0, 5, -10,
+        -20, -10, -10, -10, -10, -10, -10, -20,
     };
 
-    static constexpr int32_t BasicBlackRookPositionValues[]
+    static constexpr int16_t BasicBlackRookPositionValues[]
     {
-        0,  0,  0,  0,  0,  0,  0,  0,
-        5, 10, 10, 10, 10, 10, 10,  5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        0,  0,  5,  5,  5,  5,  0,  0
+        0, 0, 0, 0, 0, 0, 0, 0,
+        5, 10, 10, 10, 10, 10, 10, 5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        0, 0, 5, 5, 5, 5, 0, 0
     };
 
-    static constexpr int32_t BasicBlackQueenPositionValues[]
+    static constexpr int16_t BasicBlackQueenPositionValues[]
     {
-        -20,-10,-10, -5, -5,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  3,  3,  3,  3,  0,-10,
-         -5,  0,  3,  5,  5,  3,  0, -5,
-          0,  3,  5,  5,  5,  5,  3, -5,
-        -10,  5,  5,  5,  5,  5,  0,-10,
-        -10,  0,  5,  0,  0,  0,  0,-10,
-        -20,-10,-10, 1, 1,-10,-10,-20
+        -20, -10, -10, -5, -5, -10, -10, -20,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -10, 0, 3, 3, 3, 3, 0, -10,
+        -5, 0, 3, 5, 5, 3, 0, -5,
+        0, 3, 5, 5, 5, 5, 3, -5,
+        -10, 5, 5, 5, 5, 5, 0, -10,
+        -10, 0, 5, 0, 0, 0, 0, -10,
+        -20, -10, -10, 1, 1, -10, -10, -20
     };
 
-    static constexpr int32_t BasicBlackKingPositionValues[]{
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -20,-30,-30,-40,-40,-30,-30,-20,
-        -10,-20,-20,-20,-20,-20,-20,-10,
-         20, 20,  0,  0,  0,  0, 20, 20,
-         20, 30, 10,  0,  0, 10, 30, 20
+    static constexpr int16_t BasicBlackKingPositionValues[]{
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -20, -30, -30, -40, -40, -30, -30, -20,
+        -10, -20, -20, -20, -20, -20, -20, -10,
+        20, 20, 0, 0, 0, 0, 20, 20,
+        20, 30, 10, 0, 0, 10, 30, 20
     };
 
-    static constexpr const int32_t* BasicBlackPositionValues[]{
+    static constexpr const int16_t* BasicBlackPositionValues[]{
         BasicBlackPawnPositionValues, BasicBlackKnightPositionValues, BasicBlackBishopPositionValues,
-        BasicBlackRookPositionValues, BasicBlackQueenPositionValues,  BasicBlackKingPositionValues,
+        BasicBlackRookPositionValues, BasicBlackQueenPositionValues, BasicBlackKingPositionValues,
     };
 
-    static constexpr CostArrayT CostsWithPositionsIncluded = []()
+    static constexpr CostArrayT CostsWithPositionsIncluded = []() constexpr
     {
         CostArrayT arr{};
 
@@ -255,6 +197,36 @@ struct BoardEvaluator
         {
             for (size_t j = 0; j < Board::BoardFields; ++j)
                 arr[WhiteIndex + i][j] = BasicFigureValues[i] + BasicBlackPositionValues[i][j];
+        }
+
+        return arr;
+    } ();
+
+    static constexpr MaterialArrayT _materialTable = []() constexpr
+    {
+        constexpr auto _reverseMaterialIndex = [](const size_t index) constexpr -> std::array<size_t , 10>
+        {
+            return {
+                (index % BlackPawnCoef) / WhitePawnCoef,
+                (index % BlackKnightCoef) / WhiteKnightCoef,
+                (index % BlackBishopCoef) / WhiteBishopCoef,
+                (index % BlackRookCoef) / WhiteRookCoef,
+                (index % BlackQueenCoef) / WhiteQueenCoef,
+                index / BlackPawnCoef,
+                (index % WhitePawnCoef) / BlackKnightCoef,
+                (index % WhiteKnightCoef) / BlackBishopCoef,
+                (index % WhiteBishopCoef) / BlackRookCoef,
+                (index % WhiteRookCoef) / BlackQueenCoef,
+            };
+        };
+
+        MaterialArrayT arr{};
+
+        for (size_t i = 0 ; i < MaterialTableSize; ++i)
+        {
+            auto figArr = _reverseMaterialIndex(i);
+
+
         }
 
         return arr;
--- src/BoardEvaluator.cpp
+++ src/BoardEvaluator.cpp
@@ -3,3 +3,137 @@
 //
 
 #include "../include/Evaluation/BoardEvaluator.h"
+
+#include "../include/MoveGeneration/BlackPawnMap.h"
+#include "../include/MoveGeneration/WhitePawnMap.h"
+
+int32_t BoardEvaluator::PlainMaterialEvaluation(const Board& bd)
+{
+    int eval = 0;
+
+    for (size_t i = 0; i < Board::BoardsCount; ++i)
+        eval += CountOnesInBoard(bd.boards[i]) * BasicFigureValues[i];
+
+    return eval;
+}
+
+int32_t BoardEvaluator::NaiveEvaluation2(const Board& bd)
+{
+    int eval = 0;
+
+    // iterate through boards
+    for (size_t bInd = 0; bInd < Board::BoardsCount; ++bInd)
+    {
+        // extract figures board
+        uint64_t figs = bd.boards[bInd];
+
+        // iterate through figures
+        while (figs)
+        {
+            const int figPos = ExtractMsbPos(figs);
+
+            // sum costs offseted by position
+            eval += CostsWithPositionsIncluded[bInd][figPos];
+
+            // remove processed figures
+            figs ^= maxMsbPossible >> figPos;
+        }
+    }
+
+    return eval;
+}
+
+int32_t BoardEvaluator::NaiveEvaluation3(const Board& bd)
+{
+    int eval = 0;
+
+    // iterate through boards
+    for (size_t bInd = 0; bInd < Board::BoardsCount; ++bInd)
+    {
+        // extract figures board
+        uint64_t figs = bd.boards[bInd];
+
+        // iterate through figures
+        while (figs)
+        {
+            const int figPos = ExtractMsbPos(figs);
+
+            // sum costs offseted by position
+            eval += CostsWithPositionsIncluded[bInd][figPos];
+
+            // remove processed figures
+            figs ^= maxMsbPossible >> figPos;
+        }
+    }
+
+    // add bonuses for covering pawns
+    eval = _applyBonusForCoveredPawns(bd, eval);
+
+    return eval;
+}
+
+int32_t BoardEvaluator::_applyBonusForCoveredPawns(const Board& bd, int32_t eval)
+{
+    const uint64_t covereWPawns = WhitePawnMap::GetAttackFields(bd.boards[wPawnsIndex]) & bd.boards[wPawnsIndex];
+    eval += CountOnesInBoard(covereWPawns) * CoveredWPawnBonus;
+
+    const uint64_t coveredBPawns = BlackPawnMap::GetAttackFields(bd.boards[bPawnsIndex]) & bd.boards[bPawnsIndex];
+    eval += CountOnesInBoard(coveredBPawns) * CoveredBPawnBonus;
+
+    return eval;
+}
+
+size_t BoardEvaluator::_getMaterialBoardIndex(const Board& bd)
+{
+    static constexpr int OverflowTables[] = {
+        9, 3, 3, 3, 2, 0, 9, 3, 3, 3, 2, 0
+    };
+    int counts[Board::BoardsCount];
+    int overflows = 0;
+
+    for (size_t i = wPawnsIndex; i < wKingIndex; ++i)
+    {
+        counts[i] = __builtin_popcountll(bd.boards[i]);
+        overflows += counts[i] >= OverflowTables[i];
+    }
+
+    if (overflows != 0)
+        return SIZE_MAX;
+
+    for (size_t i = bPawnsIndex; i < bKingIndex; ++i)
+    {
+        counts[i] = __builtin_popcountll(bd.boards[i]);
+        overflows += counts[i] >= OverflowTables[i];
+    }
+
+    if (overflows != 0)
+        return SIZE_MAX;
+
+    return _getMaterialBoardIndex(
+        counts[bPawnsIndex],
+        counts[bKnightsIndex],
+        counts[bBishopsIndex],
+        counts[bRooksIndex],
+        counts[bQueensIndex],
+        counts[wPawnsIndex],
+        counts[wKnightsIndex],
+        counts[wBishopsIndex],
+        counts[wRooksIndex],
+        counts[wQueensIndex]
+    );
+}
+
+size_t BoardEvaluator::_getMaterialBoardIndex(const size_t P, const size_t N, const size_t B, const size_t R,
+    const size_t Q, const size_t p, const size_t n, const size_t b, const size_t r, const size_t q)
+{
+    return p * BlackPawnCoef +
+           P * WhitePawnCoef +
+           n * BlackKnightCoef +
+           N * WhiteKnightCoef +
+           b * BlackBishopCoef +
+           B * WhiteBishopCoef +
+           r * BlackRookCoef +
+           R * WhiteRookCoef +
+           q * BlackQueenCoef +
+           Q * WhiteQueenCoef;
+}
