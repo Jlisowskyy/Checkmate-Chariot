7404e1a848a0446a42f272fd3847700c8faf991e
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-04-28 21:20:11 +0200

    Finishing touches to github google test workflow

--- tests/TestingGoogleTests.cc
+++ tests/TestingGoogleTests.cc
@@ -1,11 +1,6 @@
 #include <gtest/gtest.h>
 // A simple dummy test. Trying to run it automatically with github actions.
 
-// Copied from: https://google.github.io/googletest/quickstart-cmake.html
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+TEST(GithubIntegration, DummyTest) {
+    EXPECT_EQ(1, 1) << "Github actions integration is being executed";
 }
