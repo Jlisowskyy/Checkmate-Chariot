348cfbdf57a3cc4dd549dc8126f087cd65a41774
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-05-08 15:05:13 +0200

    fixed maxwell installer path being wrong

--- EloEval/ReferenceEngines/DownloadScripts/maxwell.sh
+++ EloEval/ReferenceEngines/DownloadScripts/maxwell.sh
@@ -4,6 +4,6 @@ git clone https://github.com/eboatwright/Maxwell.git
 cd Maxwell
 git checkout 28cca0397b822237aaea42aceee990c118198b7d
 cargo build --release
-mv target/release/maxwell ../../Exes
+mv target/release/maxwell ../../Exes/
 cd ../
 rm -rf Maxwell
