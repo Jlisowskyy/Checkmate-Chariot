161d1dd246235578bee9cf335e03f28dae64ce45
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-05-12 01:27:35 +0200

    Added thread manager tests

--- tests/UCITranslator.cc
+++ tests/UCITranslator.cc
@@ -39,12 +39,51 @@ TEST(GoCommandTest, stopCommandResponse) {
 
     auto moveTimeSearch = [&]() {
         GoInfo info{};
-        info.timeInfo.moveTime = 1000;
+        info.timeInfo.moveTime = 100000;
 
         threadManager.Go(board, 0, info);
     };
 
+    auto colTimeSearch = [&]() {
+        GoInfo info{};
+        info.timeInfo.wTime = 100000;
+        info.timeInfo.bTime = 100000;
+
+        threadManager.Go(board, 0, info);
+    };
+
+    Case(colTimeSearch);
+    board = FenTranslator::GetDefault();
     Case(infiniteSearch);
+    board = FenTranslator::GetDefault();
     Case(depthSearch);
+    board = FenTranslator::GetDefault();
     Case(moveTimeSearch);
+}
+
+TEST(GoCommandTest, timeSpentTest)
+{
+    SearchThreadManager threadManager{};
+    Board board = FenTranslator::GetDefault();
+
+    auto t1 = std::chrono::steady_clock::now();
+
+    GoInfo info{};
+    info.timeInfo.moveTime = 1200;
+
+    threadManager.Go(board, 0, info);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    threadManager.Stop();
+
+    while (threadManager.IsSearchOn()) {
+        std::this_thread::sleep_for(std::chrono::microseconds (1));
+        auto t2 = std::chrono::steady_clock::now();
+
+        std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+        ASSERT_LT(time_span.count(), 2.0);
+    }
+
+    auto t2 = std::chrono::steady_clock::now();
+    ASSERT_LT(std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count(), 1200*1.1);
 }
