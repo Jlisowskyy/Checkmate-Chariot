1aab2ae613350f669dd6ed0348138c5fa2e92b97
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-05-19 15:21:16 +0200

    Reverting main changes

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <cstdlib>
 
 #include "include/ChessEngine.h"
-#include "include/TestsAndDebugging/TestSetup.h"
 
 int main(const int argc, const char **argv)
 {
-//    ChessEngineMainEntry(argc, argv);
-
-    TestSetup test{};
-    test.Initialize();
-
-    test.ProcessCommand("go infinite");
-    std::this_thread::sleep_for(std::chrono::seconds{5});
-    test.ProcessCommand("stop");
-    test.Close();
+    ChessEngineMainEntry(argc, argv);
 
     return EXIT_SUCCESS;
 }
