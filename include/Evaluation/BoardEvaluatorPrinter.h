5682593e50243e43b82905ca0d6502b22ae9404f
pietraldo <pietrzakjakub4321@gmail.com>
2024-05-20 09:13:26 +0200

    corected printing function3

--- include/Evaluation/BoardEvaluatorPrinter.h
+++ include/Evaluation/BoardEvaluatorPrinter.h
@@ -14,59 +14,62 @@ class BoardEvaluatorPrinter
     static std::array<char, 64> figureType;
 
     private:
-    template <EvalMode mode>
-    static void printBoardWithEval(const std::array<int16_t, 64> &eval)
+    template <EvalMode mode> static void printBoardWithEval(const std::array<int16_t, 64> &eval)
     {
         if constexpr (mode == EvalMode::PrintMode)
         {
-            const int fieldWidth=9;
-            GlobalLogger.LogStream << std::string(81, '-') << std::endl;
-            for (int i = 0; i < 8*2; ++i)
-            {
-                if(i%2==0)
-                {
-                    for (int j = 7; j >=0; --j)
-                    {
-                        if(figureType[i/2*8+j]!=0)
-                            GlobalLogger.LogStream << "|" << std::string((fieldWidth-1)-(fieldWidth-1)/2, ' ')<< figureType[i/2*8+j] << std::string((fieldWidth-1)/2, ' ');
-                        else
-                            GlobalLogger.LogStream << "|" << std::string(fieldWidth, ' ');
-                    }
-                    GlobalLogger.LogStream << "| " <<8-i/2<< std::endl;
-                }
-                else
-                {
-                    for (int j = 7; j >=0; --j)
-                    {
-                        if(figureType[(i-1)/2*8+j]!=0)
-                        {
-                            std::string points =((eval[i/2*8+j]>0)?"+":"")+ std::to_string(eval[i/2 * 8 + j]);
-                            int spaces=fieldWidth - points.length();
-                            int spacesLeft=spaces/2;
-                            GlobalLogger.LogStream << "|" << std::string(spacesLeft, ' ')<< points << std::string(spaces-spacesLeft, ' ');
-                        }
-                        else
-                        {
-                            GlobalLogger.LogStream << "|" << std::string(fieldWidth, ' ');
-                        }
+            const int fieldWidth  = 9;
+            const int numRows     = 8;
+            const int tableLength = fieldWidth * numRows + numRows + 1;
 
-                    }
-                    GlobalLogger.LogStream << "|" << std::endl;
-                    GlobalLogger.LogStream << std::string(81, '-') << std::endl;
-                }
-
-            }
-            for (size_t x = 0; x < 8; ++x)
+            GlobalLogger.LogStream << std::string(tableLength, '-') << std::endl;
+            for (int i = 0; i < numRows; ++i)
             {
-                GlobalLogger.LogStream << "|" << std::string((fieldWidth-1)-(fieldWidth-1)/2, ' ') << static_cast<char>('A' + x)  << std::string((fieldWidth-1)/2, ' ');
+                // printing higher row
+                for (int j = numRows - 1; j >= 0; --j)
+                {
+                    if (figureType[i * numRows + j] != 0)
+                    {
+                        int spaces     = fieldWidth - 1;
+                        int spacesLeft = spaces / 2;
+                        GlobalLogger.LogStream << "|" << std::string(spacesLeft, ' ') << figureType[i * numRows + j]
+                                               << std::string(spaces - spacesLeft, ' ');
+                    }
+                    else
+                        GlobalLogger.LogStream << "|" << std::string(fieldWidth, ' ');
+                }
+                GlobalLogger.LogStream << "| " << numRows - i << std::endl;
+
+                // printing lower row
+                for (int j = numRows - 1; j >= 0; --j)
+                {
+                    if (figureType[i * numRows + j] != 0)
+                    {
+                        std::string points =
+                            ((eval[i * numRows + j] > 0) ? "+" : "") + std::to_string(eval[i * numRows + j]);
+                        int spaces     = fieldWidth - points.length();
+                        int spacesLeft = spaces / 2;
+                        GlobalLogger.LogStream << "|" << std::string(spacesLeft, ' ') << points
+                                               << std::string(spaces - spacesLeft, ' ');
+                    }
+                    else
+                        GlobalLogger.LogStream << "|" << std::string(fieldWidth, ' ');
+                }
+                GlobalLogger.LogStream << "|" << std::endl;
+                GlobalLogger.LogStream << std::string(tableLength, '-') << std::endl;
             }
-            GlobalLogger.LogStream << "|"<<std::endl<<std::endl;
+            for (int x = 0; x < numRows; ++x)
+            {
+                int spaces     = fieldWidth;
+                int spacesLeft = spaces / 2;
+                GlobalLogger.LogStream << std::string(spacesLeft, ' ') << static_cast<char>('A' + x)
+                                       << std::string(spaces - spacesLeft, ' ');
+            }
+            GlobalLogger.LogStream << std::endl << std::endl;
         }
     }
 
-
     public:
-
     // Function prints how evaluation works when EvalMode is PrintMode
     template <EvalMode mode> static void print(const std::string &str)
     {
@@ -80,7 +83,7 @@ class BoardEvaluatorPrinter
         if constexpr (mode == EvalMode::PrintMode)
         {
             positionValue = {};
-            figureType = {};
+            figureType    = {};
         }
     }
 
@@ -93,25 +96,24 @@ class BoardEvaluatorPrinter
         }
     }
 
-    template <EvalMode mode> static void setValueOfPiecePosition(const int pieceIndex, const int16_t value,const char pieceType,const int16_t color)
+    template <EvalMode mode>
+    static void
+    setValueOfPiecePosition(const int pieceIndex, const int16_t value, const char pieceType, const int16_t color)
     {
         if constexpr (mode == EvalMode::PrintMode)
         {
-            if(color==WHITE)
+            if (color == WHITE)
             {
-                positionValue[pieceIndex]=value;
-                figureType[pieceIndex]=pieceType;
+                positionValue[pieceIndex] = value;
+                figureType[pieceIndex]    = pieceType;
             }
             else
             {
-                positionValue[pieceIndex]=value*-1;
-                figureType[pieceIndex]=pieceType+32; // change to lowercase
+                positionValue[pieceIndex] = value * -1;
+                figureType[pieceIndex]    = pieceType + 32; // change to lowercase
             }
         }
     }
-
-
-
 };
 
 #endif // CHECKMATE_CHARIOT_BOARDEVALUATORPRINTER_H
