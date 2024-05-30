599b25d67f5d7d0ecad4bb4117db70815074a079
Jlisowskyy <Jlisowskyy@gmail.com>
2024-05-30 12:53:37 +0200

    Created test file

---
+++ tests/ChessMechanics.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "../include/MoveGeneration/MoveGenerator.h"
+#include "../include/TestsAndDebugging/TestSetup.h"
+#include "../include/TestsAndDebugging/DebugTools.h"
+
+TEST(ChessMechTests, DrawTest)
+{
+
+}
