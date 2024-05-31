7f730492f84b00facb960d6855013e3e343b166a
Jlisowskyy <Jlisowskyy@gmail.com>
2024-05-31 19:33:42 +0200

    Added test breaking ZHasher

--- tests/SearchTests.cc
+++ tests/SearchTests.cc
@@ -8,17 +8,20 @@
 #include "../include/TestsAndDebugging/DebugTools.h"
 #include "../include/Search/BestMoveSearch.h"
 #include "../include/Search/ZobristHash.h"
+#include "../include/ParseTools.h"
 
 TEST(TranspositionTableTests, HashFunctionTest1)
 {
-//    return;
-
     std::vector<std::string> posCommand{
-        "position fen r2q2k1/1ppbb3/p3pr2/1P1p2p1/3P2P1/P1NQ1N2/2P2P2/R3K2R w KQ - 0 18"
+        "position fen r2q2k1/1ppbb3/p3pr2/1P1p2p1/3P2P1/P1NQ1N2/2P2P2/R3K2R w KQ - 0 18",
+        "position startpos"
     };
+
+
 
     std::vector<std::vector<std::string>> movesSubCommands{
         {"d3h7", "g8f8", "h7h8", "f8f7"},
+        ParseTools::Split("g1f3 b7b6 e2e4 c8b7 b1c3 e7e6 a2a3 g8f6 e4e5 f6d5 f1c4 d5c3 d2c3 d7d6 c1g5 f8e7 g5e7 d8e7 e5d6 c7d6 e1g1 e8g8"),
     };
 
     for (size_t i = 0; i < posCommand.size(); ++i)
